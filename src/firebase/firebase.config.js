0694ad802702e076cbe4a64c51ffc22966ed1be8
Rakibul Islam <rakibulrakib05@gmail.com>
2023-08-21 13:05:04 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCuAG0P5eOfX672vrJZVX-DAJlnSGRo278",
+  authDomain: "auth-firebase-context-ta-82a5b.firebaseapp.com",
+  projectId: "auth-firebase-context-ta-82a5b",
+  storageBucket: "auth-firebase-context-ta-82a5b.appspot.com",
+  messagingSenderId: "503582547850",
+  appId: "1:503582547850:web:5c429652ded12be3bef273"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/providers/__steps____.js
@@ -0,0 +1,17 @@
+/**
+ * context api
+ * 1.creater context (export)
+ * 2.create provider
+ * 3.set context value 
+ * 4. to acces the vlaue : usecontext hook
+ */
+
+/**
+ * 1.mian.jsx set AuthProvider
+ * 2. inside provider access children props and then use
+ */
+
+/**
+ * 1.break down the create user function
+ * 2.
+ */
